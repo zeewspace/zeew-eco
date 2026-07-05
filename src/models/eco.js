389e610221b10f9cfb00abf41e1564c1b2e728a3
Kamerr Ezz <kamerr_oficial@hotmail.com>
2026-07-04 23:09:45 -0500

    refactor!: remove Mongoose models and legacy JS source

--- src/models/eco.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
-const Economia = new Schema({
-  user: { type: String },
-  guild: { type: String },
-  money: { type: Number },
-});
-module.exports = mongoose.model("Economia ", Economia);
